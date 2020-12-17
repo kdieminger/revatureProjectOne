f2ed648484d4e8ce7f6e6340745192e160407574
connellrobert <robert.lel.connell@gmail.com>
2020-12-17 17:12:56 -0600

    working with oop in js

--- examples/week1/oop.js
+++ examples/week1/oop.js
@@ -6,11 +6,12 @@
  * encapsulation
  *      restricting direct access to data
  * inheritance
- *      
+ *      js uses prototype inheritance. if we change the prototype, we change everything
+ *          that uses the prototype.
  * polymorphism
- * 
+ *      We can use function overriding in js to create multiple different implementations.
  * abstraction
- *      
+ *       hiding implementation of certain functionality
  */
 
 
@@ -85,3 +86,41 @@ const human = new Human('sarah', 29);
 console.log(human.name);
 human.age=10;
 console.log(human.age);
+
+
+/**
+ * Although not the only way, we can create a level of abstraction through exporting.
+ *      Abstraction is the act of hiding implementation. By exporting an object for others
+ *      to work with, we are keeping the under lying implementations hidden and only revealing 
+ *      certain functions and objects to work with.
+ */
+module.exports=human;
+
+
+const skeleton = {
+    bones: 3200,
+    scare: function () {
+        console.log("I have too many bones in my body!!!!");
+    }
+}
+
+skeleton.scare();
+//we assign a new skeleton
+const newSkeleton = Object.create(skeleton);
+//we change the number of bones on the second skeleton
+newSkeleton.bones=10;
+//and we print the original skeleton bones, how many print?
+console.log(skeleton.bones);
+
+//does the new skeleton have the same property as the original?
+newSkeleton.scare();
+newSkeleton.__proto__.bones=99
+//if we print the prototype, what prints? if we change something on the prototype, does
+//      it change the original object?
+console.log(newSkeleton.__proto__);
+console.log(skeleton.bones);
+
+//classes are sugar syntax for functions that create objects. if you print
+//      typeof className, you get a function. classes will give you a new object with
+//      the object prototype.
+console.log(human.__proto__);
