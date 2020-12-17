bcd41a45a1d978f1397d22b9d901597082f12342
Katrina <katdiemin@gmail.com>
2020-12-17 12:44:16 -0500

    worked on login function

--- projectZero/kdiemingerprojectzero/src/main.js
+++ projectZero/kdiemingerprojectzero/src/main.js
@@ -2,27 +2,46 @@ const readline = require('readline');
 const { stdin } = require('process');
 const { stdout } = require('process');
 
-readline.createInterface({
+import { userLogin } from './userfunctions.js';
+
+const read = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-function start(){
-    console.log('Welcome! Please log in.');
-    userLogin();
+console.log('Welcome! Please log in.');
+logUser();
+
+function tryAgain(){
+    read.question('Try Again: Yes | No', (answer) => {
+        if (answer === "Yes" || answer === "yes"){
+            userLogin();
+        }
+        else if (answer === "No" || answer === "no"){
+            process.exit();
+        }
+        else {
+            console.log ('Error: Invalid response.');
+            tryAgain();
+        }
+    });
 }
 
-function userLogin(){
+function logUser(){
     read.question('Username:', (username) => {
         read.question('Password:', (password) => {
             let login = userLogin(username, password);
             if (login){
                 inUser = user;
                 console.log(`Welcome back ${inUser.username}!`);
+                process.exit();
             }
             else {
                 console.log('Login failed. Incorrect username or password.');
+                tryAgain();
             }
         })
-    })
+    });
 }
+
+process.exit();
