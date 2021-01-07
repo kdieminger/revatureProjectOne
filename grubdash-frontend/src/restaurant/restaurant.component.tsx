9f0f8dcbc1a0f262d69c742d02dadc86ceb3a786
Richard Orr <rlayneorr@gmail.com>
2021-01-07 17:07:43 -0500

    restaurant image click

--- grubdash-frontend/src/restaurant/restaurant.component.tsx
+++ grubdash-frontend/src/restaurant/restaurant.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './restaurant.css';
 import { Restaurant } from './restaurant';
 
@@ -7,26 +7,32 @@ interface RestaurantProps {
     data: Restaurant;
 }
 
-// Function Component
 function RestaurantComponent(props: RestaurantProps) {
+    const history = useHistory();
+
+    function goToRestaurant() {
+        history.push('/restaurants/'+props.data.name);
+    }
+
     return (
-        
         <div className='col restaurant card'>
             <img
                 src={props.data.img}
                 className='card-img-top rest-logo'
                 alt={props.data.name}
+                onClick={goToRestaurant}
             />
             <div className='card-body'>
                 <p className=''>{props.data.name}</p>
                 <p className='deliverytime'>{props.data.eta}</p>
                 <p className='rating'>{props.data.rating}</p>
                 <p className='foodtype'>{props.data.type}</p>
-            <Link to={`/restaurants/${props.data.name}`} >   See more info </Link>
+                <Link to={`/restaurants/${props.data.name}`}>
+                    {' '}
+                    See more info{' '}
+                </Link>
             </div>
         </div>
-
-        
     );
 }
 
--- grubdash-frontend/src/user/login.component.tsx
+++ grubdash-frontend/src/user/login.component.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent } from 'react';
+import React, { SyntheticEvent, useEffect } from 'react';
 import { User } from './user';
 import userService from './user.service';
 import UserContext from '../user.context';
@@ -25,6 +25,11 @@ function LoginComponent(props: any) {
             history.push('/restaurants');
         });
     }
+    useEffect(() => {
+        userService.getLogin().then((user)=> {
+            setUserContext(user);
+        })
+    }, []);
     return (
         
         <div className='col restaurant card'>
