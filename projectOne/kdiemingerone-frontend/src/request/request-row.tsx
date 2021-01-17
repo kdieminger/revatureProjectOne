a890403d35ee44ac1841da8d07879e6e1fa55b71
Katrina <katdiemin@gmail.com>
2021-01-16 19:53:37 -0500

    app process

--- projectOne/kdiemingerone-frontend/src/request/request-row.tsx
+++ projectOne/kdiemingerone-frontend/src/request/request-row.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import RequestComponent from './request.component';
 import { AppRequest } from './request';
-import userService from '../user/user.service';
 import { useDispatch, useSelector } from 'react-redux';
 import { RequestState, UserState } from '../reducer';
 import { useHistory } from 'react-router-dom';
 import requestService from './request.service';
 import { changeRequest } from '../actions';
 
-type PropType = { request: AppRequest};
+type PropType = { request: AppRequest };
 
 
-function ReqRow(props: PropType){
+function ReqRow(props: PropType) {
     const history = useHistory();
     const dispatch = useDispatch();
     const userSelector = (state: UserState) => state.user;
@@ -19,13 +16,24 @@ function ReqRow(props: PropType){
     const requestSelector = (state: RequestState) => state.request;
     const request = useSelector(requestSelector);
 
-    function approveRequest(){
+    function approveRequest() {
         dispatch(changeRequest(props.request));
         props.request.approval.push(true);
+        props.request.appStatus = 'denied';
         dispatch(changeRequest(props.request));
         requestService.updateRequest(request).then(() => {
             console.log('updating request');
-            history.push('/users/'+user.username);
+            history.push('/home');
+        })
+    }
+
+    function denyRequest() {
+        dispatch(changeRequest(props.request));
+        props.request.approval.push(false);
+        dispatch(changeRequest(props.request));
+        requestService.updateRequest(request).then(() => {
+            console.log('updating request');
+            history.push('/home')
         })
     }
 
@@ -34,23 +42,18 @@ function ReqRow(props: PropType){
     return (
         <section className="row border">
             <table className='table'>
-                <tbody> 
-                <tr>
-                    <td>{props.request.requestID}</td>
-                    <td>{props.request.username}</td>
-                    <td>{props.request.type}</td>
-                    <td>{props.request.date}</td>
-                    <td>{props.request.time}</td>
-                    <td>{props.request.location}</td>
-                    <td>{props.request.description}</td>
-                    <td>{props.request.cost}</td>
-                    <td>{props.request.justification}</td>
-                </tr>
-                {user.role != 'Employee' && user.username != props.request.username &&(
+                <tbody>
                     <tr>
-                        <td><button className= 'btn' onClick={approveRequest}>Approve</button></td>
+                        <td>{props.request.requestID}</td>
+                        <td>{props.request.username}</td>
+                        <td>{props.request.type}</td>
+                        <td>{props.request.date}</td>
+                        <td>{props.request.time}</td>
+                        <td>{props.request.location}</td>
+                        <td>{props.request.description}</td>
+                        <td>{props.request.cost}</td>
+                        <td>{props.request.justification}</td>
                     </tr>
-                )}
                 </tbody>
             </table>
         </section>
---
+++ projectOne/kdiemingerone-frontend/src/request/sup-request-row.tsx
@@ -0,0 +1,120 @@
+import { AppRequest } from './request';
+import { useDispatch, useSelector } from 'react-redux';
+import { RequestState, UserState } from '../reducer';
+import { useHistory } from 'react-router-dom';
+import requestService from './request.service';
+import { changeRequest } from '../actions';
+
+type PropType = { request: AppRequest };
+
+
+function SupReqRow(props: PropType) {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const userSelector = (state: UserState) => state.user;
+    const user = useSelector(userSelector);
+    const requestSelector = (state: RequestState) => state.request;
+    const request = useSelector(requestSelector);
+
+    function approveRequest() {
+        dispatch(changeRequest(props.request));
+        props.request.approval.push(true);
+        if (props.request.approval.length == 3) {
+            props.request.appStatus = 'approved';
+        }
+        dispatch(changeRequest(props.request));
+        requestService.updateRequest(request).then(() => {
+            console.log('updating request');
+            history.push('/home');
+        })
+    }
+
+    function denyRequest() {
+        dispatch(changeRequest(props.request));
+        props.request.approval.push(false);
+        props.request.appStatus = 'denied';
+        dispatch(changeRequest(props.request));
+        requestService.updateRequest(request).then(() => {
+            console.log('updating request');
+            history.push('/home')
+        })
+    }
+
+    console.log(request.approval);
+
+    return (
+        <section className="row border">
+            {user.role === 'Supervisor' && props.request.approval.length == 0 && (
+                <table className='table'>
+                    <tbody>
+                        <tr>
+                            <td>{props.request.requestID}</td>
+                            <td>{props.request.username}</td>
+                            <td>{props.request.type}</td>
+                            <td>{props.request.date}</td>
+                            <td>{props.request.time}</td>
+                            <td>{props.request.location}</td>
+                            <td>{props.request.description}</td>
+                            <td>{props.request.cost}</td>
+                            <td>{props.request.justification}</td>
+                        </tr>
+                        <div>
+                            <tr>
+                                <td><button className='btn' onClick={approveRequest}>Approve</button></td>
+                                <td><button className='btn' onClick={denyRequest}>Deny</button></td>
+                            </tr>
+                        </div>
+                    </tbody>
+                </table>
+            )}
+            {user.role === 'Department Head' && props.request.approval.length == 1 && props.request.appStatus !== 'denied' && (
+                <table className='table'>
+                    <tbody>
+                        <tr>
+                            <td>{props.request.requestID}</td>
+                            <td>{props.request.username}</td>
+                            <td>{props.request.type}</td>
+                            <td>{props.request.date}</td>
+                            <td>{props.request.time}</td>
+                            <td>{props.request.location}</td>
+                            <td>{props.request.description}</td>
+                            <td>{props.request.cost}</td>
+                            <td>{props.request.justification}</td>
+                        </tr>
+                        <div>
+                            <tr>
+                                <td><button className='btn' onClick={approveRequest}>Approve</button></td>
+                                <td><button className='btn' onClick={denyRequest}>Deny</button></td>
+                            </tr>
+                        </div>
+                    </tbody>
+                </table>
+            )}
+            {user.role === 'BenCo' && props.request.approval.length == 2 && props.request.appStatus !== 'denied' && (
+                <table className='table'>
+                    <tbody>
+                        <tr>
+                            <td>{props.request.requestID}</td>
+                            <td>{props.request.username}</td>
+                            <td>{props.request.type}</td>
+                            <td>{props.request.date}</td>
+                            <td>{props.request.time}</td>
+                            <td>{props.request.location}</td>
+                            <td>{props.request.description}</td>
+                            <td>{props.request.cost}</td>
+                            <td>{props.request.justification}</td>
+                        </tr>
+                        <div>
+                            <tr>
+                                <td><button className='btn' onClick={approveRequest}>Approve</button></td>
+                                <td><button className='btn' onClick={denyRequest}>Deny</button></td>
+                            </tr>
+                        </div>
+                    </tbody>
+                </table>
+            )}
+        </section>
+    )
+}
+
+export default SupReqRow;
