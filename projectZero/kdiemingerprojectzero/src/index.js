4b00b184383eb0233078db95f806096b2d4a8653
kdieminger <77751790+kdieminger@users.noreply.github.com>
2021-02-04 18:49:05 -0500

    Delete projectZero directory

--- projectZero/kdiemingerprojectzero/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var main_1 = require("./main");
-//load();
-main_1.start();
--- projectZero/kdiemingerprojectzero/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { start } from './main';
-import logger from './log';
-import { calcMonthPay } from './offer/offer';
-
-//load();
-start();
